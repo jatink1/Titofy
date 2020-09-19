b616e8b54883587e4e0fa4e126dfb6983e511fd0
maw1a <ahmedmawia.dev@gmail.com>
2020-09-19 14:15:21 +0530

    auth flow done

--- server/app.py
+++ server/app.py
@@ -23,17 +23,21 @@ def signup():
   password = _json['password']
 
   if _json and name and email and password and len(password)>=8:
-    # Check same email throw err --not done
+    # Check same email throw err
+    checkEmail = users.find_one({ 'email': email })
+    if checkEmail:
+      response = jsonify("Email already in use!")
+      response.status_code = 409
+      return response
 
-    # hash and store in MongoDB --done
+    # hash and store in MongoDB
     hashed = hashlib.sha256(_json['password'].encode('utf-8')).hexdigest()
     user = {
       "name": name,
       "email": email,
       "password": hashed
     }
-    objid = users.insert_one(user)
-
+    objid = users.insert_one(user).inserted_id
     # generate JWT token
     token = jwt.encode({ 'email': email, 'id': str(objid) }, app.config["SECRET_KEY"])
 
@@ -44,7 +48,7 @@ def signup():
     return response
   
   # Response 401
-  response = jsonify("Couldn't verify!")
+  response = jsonify("Couldn't signup!")
   response.status_code = 401
   return response
 
@@ -54,17 +58,31 @@ def login():
 
   if auth and len(auth['email'])>=3 and len(auth['password'])>=8:
     # get document from db and verify then send jwt
-    objid = '123456'
-    token = jwt.encode({ 'email': auth['email'], 'id': objid }, app.config["SECRET_KEY"])
-    message = {'message': 'User logged in successfully', 'token': token.decode('UTF-8')}
-    response = jsonify(message)
-    response.status_code = 200
-    return response
+    hashed = hashlib.sha256(auth['password'].encode('utf-8')).hexdigest()
+    user = users.find_one({ 'email': auth['email'], 'password': hashed })
+    if user:
+      # generate JWT token
+      objid = user['_id']
+      token = jwt.encode({ 'email': auth['email'], 'id': str(objid) }, app.config["SECRET_KEY"])
+      message = {'message': 'User logged in successfully', 'token': token.decode('UTF-8')}
+      # Response 200
+      response = jsonify(message)
+      response.status_code = 200
+      return response
+    else:
+      # Response 201
+      response = jsonify("Incorrect email or password!")
+      response.status_code = 201
+      return response
 
+  # Response 401
   response = jsonify("Couldn't verify!")
   response.status_code = 401
   return response
 
+@app.route('/update', methods=['POST'])
+def update():
+  return "Update Profile"
 
 if __name__=="__main__":
   app.run(debug=True)
